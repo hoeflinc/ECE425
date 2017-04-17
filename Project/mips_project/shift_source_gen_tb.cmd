9b44c86949ed26b587fad287e5fcc2e46d8519b5
millerid <millerid@localhost.localdomain>
2017-04-17 19:58:16 -0400

    Finished source generation hw

---
+++ Project/mips_project/shift_source_gen_tb.cmd
@@ -0,0 +1,16 @@
+h Vdd!
+l Gnd!
+vector a a7 a6 a5 a4 a3 a2 a1 a0
+vector z z14 z13 z12 z11 z10 z9 z8 z7 z6 z5 z4 z3 z2 z1 z0
+analyzer a z right arith rightandarith
+setvector a 10101010
+stepsize 100
+l right
+l arith
+s
+h right
+s
+h arith
+s
+l right
+s
