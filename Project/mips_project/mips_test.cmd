5d0b346abe1c4cb55eb5f326c5becf9d6e618354
millerid <millerid@localhost.localdomain>
2017-05-02 16:05:25 -0400

    incremental

--- Project/mips_project/mips_test.cmd
+++ Project/mips_project/mips_test.cmd
@@ -5,14 +5,16 @@ l Gnd!
 vector adr adr7 adr6 adr5 adr4 adr3 adr2 adr1 adr0
 vector memdata memdata7 memdata6 memdata5 memdata4 memdata3 memdata2 memdata1 memdata0
 vector writedata writedata7 writedata6 writedata5 writedata4 writedata3 writedata2 writedata1 writedata0
-clock ph1 0 1 0 0
-clock ph2 0 0 0 1
-analyzer reset ph1 ph2 adr memdata writedata memwrite
+clock ph1 0 1 0 0 0
+clock ph2 0 0 0 1 0
+vector op op5 op4 op3 op2 op1 op0
+analyzer reset ph1 ph2 adr memdata writedata memwrite op
 
 #reset everything
 h reset
 c 2
 l reset
+c 1
 
 #Load 0x5 into v0 (80020044)
 #First 4 clocks we cycle in the instruction
